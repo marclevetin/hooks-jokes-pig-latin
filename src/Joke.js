0b75fbc6487ea0bacfaee8f441c8d4ce30dcc127
marclevetin <marc@derbyhat.org>
2019-06-16 00:50:55 -0400

    Added punctuation support.  Fixed bug for doubled spaces between sentences

--- src/Joke.js
+++ src/Joke.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { start } from 'repl';
 
 function Joke() {
   const [joke, setJoke] = useState('');
@@ -16,30 +17,67 @@ function Joke() {
   }
 
   const basicPigLatinatron = (string) => {
-      // TODO refactor for punctuation, quotes at the end of words
-    return string.split(' ')
-                .map(word => {
-                    const vowels = ['a','e','i','o','u'];
-                    const firstLetter = word[0].toLowerCase();
-                    const secondLetter = (word.length > 1) ? word[1].toLowerCase() : null;
+    // rules of Pig Latin: https://web.ics.purdue.edu/~morelanj/RAO/prepare2.html  
+    // TODO refactor for punctuation, quotes at the end of words
+    try {
+        return string.split(' ')
+                    .map(word => {
+                        if (word.length === 0) return word; // covers jokes that have two sentences and a double space after the first sentence ends
 
-                    const isCapitalized = (firstLetter !== word[0]);
-                    const beginsWithVowel = vowels.includes(firstLetter);
-                    const secondLetterConsonant = !vowels.includes(secondLetter);
+                        let workingWord = word;
+                        const vowels = ['a','e','i','o','u'];
+                        const punctuation = `"'.,?`;
 
-                    let returnWord = '';
+                        let startingPunctuation = null;
+                        let endingPunctuation = null;
 
-                    if (beginsWithVowel) {
-                        returnWord = `${word}way`;
-                    } else if (!beginsWithVowel && secondLetterConsonant) {
-                        returnWord = `${word.slice(2)}${firstLetter}${secondLetter}ay`;
-                    } else {
-                        returnWord = `${word.slice(1)}${firstLetter}ay`;
-                    }
+                        if (punctuation.includes(word[0])) {
+                            startingPunctuation = word[0];
+                            workingWord = word.slice(1);
+                        }
 
-                    return (isCapitalized) ? `${returnWord[0].toUpperCase()}${returnWord.slice(1)}` : `${returnWord}`;
-                })
-                .join(' ');
+                        if (punctuation.includes(word[word.length-1])) {
+                            endingPunctuation = word[word.length-1];
+                            workingWord = workingWord.substring(0, workingWord.length - 1);
+                        }
+
+                        const firstLetter = workingWord[0].toLowerCase();
+                        const secondLetter = (workingWord.length > 1) ? workingWord[1].toLowerCase() : null;
+
+                        const isCapitalized = (firstLetter !== workingWord[0]);
+                        const beginsWithVowel = vowels.includes(firstLetter);
+                        const secondLetterConsonant = !vowels.includes(secondLetter);
+
+                        let returnWord = '';
+
+                        if (beginsWithVowel) {
+                            returnWord = `${workingWord}way`;
+                        } else if (!beginsWithVowel && secondLetterConsonant) {
+                            returnWord = `${workingWord.slice(2)}${firstLetter}${secondLetter}ay`;
+                        } else {
+                            returnWord = `${workingWord.slice(1)}${firstLetter}ay`;
+                        }
+                        
+                        if (isCapitalized) {
+                            returnWord = `${returnWord[0].toUpperCase()}${returnWord.slice(1)}`
+                        }
+
+                        if (startingPunctuation) {
+                            returnWord = `${startingPunctuation}${returnWord}`;
+                        }
+
+                        if (endingPunctuation) {
+                            returnWord = `${returnWord}${endingPunctuation}`;
+                        }
+
+                        return returnWord;
+                    })
+                    .join(' ');
+    }
+    catch(error) {
+        console.error('Problem with this joke:', arguments[0]);
+        console.error(error.message);
+    }
   }
 
   const printableJoke = pigLatinStatus ? basicPigLatinatron(joke) : joke;
